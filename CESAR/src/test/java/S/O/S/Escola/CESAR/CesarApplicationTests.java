16fe1e70eb2a97069732ea279daf01902289dac2
jleandromorais <leandromorais12345@outlook.com>
2025-09-27 10:03:14 -0300

    Primeiro commit do projeto CESAR

---
+++ CESAR/src/test/java/S/O/S/Escola/CESAR/CesarApplicationTests.java
@@ -0,0 +1,13 @@
+package S.O.S.Escola.CESAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
