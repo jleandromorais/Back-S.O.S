f1bcbabf755e58e3942e2b5ad503fa730a91b8f4
Gabriel Souza <gabriel123.souza06@gmail.com>
2025-09-29 01:55:10 -0300

    feat: Adiciona endpoint GET para Denuncias e corrige bugs de DTOs e Services.

---
+++ CESAR/src/main/java/S/O/S/Escola/CESAR/Repository/DenunciaRepository.java
@@ -0,0 +1,30 @@
+package S.O.S.Escola.CESAR.Repository;
+
+import S.O.S.Escola.CESAR.Model.Denuncia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
+
+    // --- MÉTODOS DE CONSULTA PERSONALIZADOS (OPCIONAL) ---
+    // O Spring Data JPA cria a consulta SQL automaticamente a partir do nome do método.
+
+    /**
+     * Encontra todas as denúncias com um status específico.
+     * Exemplo de uso: denunciaRepository.findByStatus("Em Análise");
+     * @param status O status a ser pesquisado.
+     * @return Uma lista de denúncias que correspondem ao status.
+     */
+    List<Denuncia> findByStatus(String status);
+
+    /**
+     * Encontra todas as denúncias de um tipo específico.
+     * Exemplo de uso: denunciaRepository.findByTipo(TipoDenuncia.BULLYING);
+     * @param tipo O tipo da denúncia.
+     * @return Uma lista de denúncias daquele tipo.
+     */
+
+}
---
+++ src/main/java/br.com.sosescolar/CESAR/DTO/DenunciaDTO.java
@@ -0,0 +1,29 @@
+package S.O.S.Escola.CESAR.DTO;
+
+import S.O.S.Escola.CESAR.Enum.TipoDeDenun;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.time.LocalDateTime;
+@Data
+public class DenunciaDTO {
+    // Campos que serão retornados na resposta
+    private Long id;
+    private String status;
+    private LocalDateTime dataCriacao;
+    private AlunoDTO autor;
+
+    // Campos que vêm na requisição POST
+    @NotNull
+    private TipoDeDenun tipo;
+    @NotBlank
+    @Size(min = 5, max = 100)
+    private String titulo;
+    @NotBlank @Size(min = 20)
+    private String descricao;
+    @NotNull
+    private Boolean anonima;
+    private Long autorId;
+}
